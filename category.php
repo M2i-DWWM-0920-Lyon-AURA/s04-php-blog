0f103b9c96efa364309c24b632f6182afe1c114a
Romain Tello <romain.tello@gmail.com>
2020-10-14 17:03:00 +0200

    Refactot single category page using Category class

--- category.php
+++ category.php
@@ -1,7 +1,31 @@
-<?php include './data/articles.php'; ?>
-<?php include './data/categories.php'; ?>
+<?php include './utils/database.php' ?>
+<?php include './class/Category.php' ?>
 
-<?php $category = findCategoryById($_GET['id']); ?>
+<?php
+
+// Récupère la catégorie correspondant à l'ID demandé
+$result = fetchFromDatabase('
+SELECT *
+    FROM `categories`
+    WHERE `id` = ' . $_GET['id']
+);
+
+// Si le résultat de la requête est un tableau vide, c'est donc qu'il n'existe pas de catégorie
+// avec l'ID demandé
+if (empty($result)) {
+    throw new Error('Category #' . $_GET['id'] . ' does not exist.');
+// Sinon
+} else {
+    $categoryData = $result[0];
+
+    $category = new Category(
+        $categoryData['id'],
+        $categoryData['name'],
+        $categoryData['description']
+    );
+}
+
+?>
 
 <?php include './templates/head.tpl.php'; ?>
 
--- templates/category.tpl.php
+++ templates/category.tpl.php
@@ -1,11 +1,11 @@
 <div class="jumbotron">
-    <h1>Category: <?= $category['name'] ?></h1>
+    <h1>Category: <?= $category->getName() ?></h1>
     <div>
-        <?= $category['description'] ?>
+        <?= $category->getDescription() ?>
     </div><a href="/category-list.php"><button type="button" class="btn btn-secondary">All Categories</button></a>
 </div>
 
-<h2>Articles listed under <?= $category['name'] ?></h2>
+<h2>Articles listed under <?= $category->getName() ?></h2>
 
 <ul style="padding-inline-start: 0px; display: grid; grid-template-columns: repeat(2, 1fr); gap: 1em;">
 
