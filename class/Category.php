e515bc24ec017cb7b5c12586bf30f5169c194874
Romain Tello <romain.tello@gmail.com>
2020-10-14 17:02:45 +0200

    Add Category class

---
+++ class/Category.php
@@ -0,0 +1,67 @@
+<?php
+
+class Category
+{
+    private $id;
+    private $name;
+    private $description;
+
+    public function __construct(
+        int $id,
+        string $name,
+        string $description
+    )
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */ 
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
